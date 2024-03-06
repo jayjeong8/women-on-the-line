7dd3d9a437bb094a66d88856768c338eb4ed7c3c
jayjeong8 <zuni4242@gmail.com>
2024-03-06 23:21:54 +0900

    style: 배경 brush artwork 디자인 시안에 맞게 조정
    - 디자인 시안에 맞게 brush artwork 사이즈 변경
    - translate를 이용하여 디자인 시안에 맞게 위치 세부 조정

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,40 @@ export default function Home() {
     return (
         <main>
             <section className="grid grid-cols-3 grid-rows-3 h-screen">
-                <div className="col-start-1 col-span-1 row-start-1 row-span-1 relative">
-                    <Image className="object-contain" src="/images/sources/mix_brush.png" alt="" fill/>
+                <div className="col-start-1 col-span-1 row-start-1 row-span-1">
+                    <Image className="max-w-5xl -translate-x-12 -translate-y-2/3"
+                           src="/images/sources/mix_brush.png"
+                           alt="" width="900" height="0"/>
                 </div>
-                <div className="col-start-3 col-span-1 row-start-1 row-span-1 relative">
-                    <Image className="object-contain" src="/images/sources/blue_brush_3.png" alt="" fill/>
+                <div className="col-start-3 col-span-1 row-start-1 row-span-1">
+                    <Image className="max-w-6xl -translate-x-1/3 -translate-y-2/3"
+                           src="/images/sources/blue_brush_3.png"
+                           alt="" width="1200" height="0"/>
                 </div>
-                <div className="col-start-1 col-span-1 row-start-2 row-span-1 relative">
-                    <Image className="object-contain" src="/images/sources/blue_brush.png" alt="" fill/>
+                <div className="col-start-1 col-span-1 row-start-2 row-span-1">
+                    <Image className="max-w-2xl -translate-x-1/2 -translate-y-44"
+                           src="/images/sources/blue_brush.png"
+                           alt="" width="700" height="0"/>
                 </div>
-                <div className="col-start-3 col-span-1 row-start-2 row-span-1 relative">
-                    <Image className="object-contain" src="/images/sources/green_brush.png" alt="" fill/>
+                <div className="col-start-3 col-span-1 row-start-2 row-span-1">
+                    <Image className="max-w-3xl"
+                           src="/images/sources/green_brush.png"
+                           alt="" width="550" height="0"/>
                 </div>
-                <div className="col-start-1 col-span-1 row-start-3 row-span-1 relative">
-                    <Image className="object-contain" src="/images/sources/pink_brush.png" alt="" fill/>
+                <div className="col-start-1 col-span-1 row-start-3 row-span-1">
+                    <Image className="max-w-4xl -translate-x-1/2 translate-y-1/2"
+                           src="/images/sources/pink_brush.png"
+                           alt="" width="1000" height="0"/>
                 </div>
-                <div className="col-start-2 col-span-1 row-start-3 row-span-1 relative">
-                    <Image className="object-contain" src="/images/sources/yellow_brush.png" alt="" fill/>
+                <div className="col-start-2 col-span-1 row-start-3 row-span-1">
+                    <Image className="max-w-3xl -translate-x-1/2 -translate-y-28"
+                           src="/images/sources/yellow_brush.png"
+                           alt="" width="1100" height="0"/>
                 </div>
-                <div className="col-start-3 col-span-1 row-start-3 row-span-1 relative">
-                    <Image className="object-contain" src="/images/sources/blue_brush_2.png" alt="" fill/>
+                <div className="col-start-3 col-span-1 row-start-3 row-span-1">
+                    <Image className="max-w-5xl translate-x-24 -translate-y-24"
+                           src="/images/sources/blue_brush_2.png"
+                           alt="" width="1000" height="0"/>
                 </div>
             </section>
         </main>
