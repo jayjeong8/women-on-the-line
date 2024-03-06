f16759b2781b028b5662b71006a9aa69cf644f15
jayjeong8 <zuni4242@gmail.com>
2024-03-06 23:57:59 +0900

    style: 해상도에 상관없이 brush artwork를 일정한 위치에 배치
    - 7dd3d9a4 작업처럼 절대값 width 및 translate를 사용하면 화면 배율에 따라 의도하지 않은 위치에 brush artwork가 배치됨
    - 부모 컨테이너를 채우는 fill 및 상대값 translate를 이용하여 일정한 위치에 배치되도록 개선

--- app/page.tsx
+++ app/page.tsx
@@ -4,40 +4,40 @@ export default function Home() {
     return (
         <main>
             <section className="grid grid-cols-3 grid-rows-3 h-screen">
-                <div className="col-start-1 col-span-1 row-start-1 row-span-1">
-                    <Image className="max-w-5xl -translate-x-12 -translate-y-2/3"
+                <div className="col-start-1 col-span-1 row-start-1 row-span-1 relative">
+                    <Image className="object-contain scale-150 -translate-y-2/3"
                            src="/images/sources/mix_brush.png"
-                           alt="" width="900" height="0"/>
+                           alt="" fill/>
                 </div>
-                <div className="col-start-3 col-span-1 row-start-1 row-span-1">
-                    <Image className="max-w-6xl -translate-x-1/3 -translate-y-2/3"
+                <div className="col-start-3 col-span-1 row-start-1 row-span-1 relative">
+                    <Image className="object-contain scale-150 translate-x-1/4 -translate-y-1/3"
                            src="/images/sources/blue_brush_3.png"
-                           alt="" width="1200" height="0"/>
+                           alt="" fill/>
                 </div>
-                <div className="col-start-1 col-span-1 row-start-2 row-span-1">
-                    <Image className="max-w-2xl -translate-x-1/2 -translate-y-44"
+                <div className="col-start-1 col-span-1 row-start-2 row-span-1 relative">
+                    <Image className="object-contain scale-150 -translate-x-1/3"
                            src="/images/sources/blue_brush.png"
-                           alt="" width="700" height="0"/>
+                           alt="" fill/>
                 </div>
-                <div className="col-start-3 col-span-1 row-start-2 row-span-1">
-                    <Image className="max-w-3xl"
+                <div className="col-start-3 col-span-1 row-start-2 row-span-1 relative">
+                    <Image className="object-contain scale-150 translate-y-1/4"
                            src="/images/sources/green_brush.png"
-                           alt="" width="550" height="0"/>
+                           alt="" fill/>
                 </div>
-                <div className="col-start-1 col-span-1 row-start-3 row-span-1">
-                    <Image className="max-w-4xl -translate-x-1/2 translate-y-1/2"
+                <div className="col-start-1 col-span-1 row-start-3 row-span-1 relative">
+                    <Image className="object-contain scale-150 -translate-x-2/3 translate-y-2/3"
                            src="/images/sources/pink_brush.png"
-                           alt="" width="1000" height="0"/>
+                           alt="" fill/>
                 </div>
-                <div className="col-start-2 col-span-1 row-start-3 row-span-1">
-                    <Image className="max-w-3xl -translate-x-1/2 -translate-y-28"
+                <div className="col-start-2 col-span-1 row-start-3 row-span-1 relative">
+                    <Image className="object-contain scale-150 -translate-x-1/2 translate-y-1/4"
                            src="/images/sources/yellow_brush.png"
-                           alt="" width="1100" height="0"/>
+                           alt="" fill/>
                 </div>
-                <div className="col-start-3 col-span-1 row-start-3 row-span-1">
-                    <Image className="max-w-5xl translate-x-24 -translate-y-24"
+                <div className="col-start-3 col-span-1 row-start-3 row-span-1 relative">
+                    <Image className="object-contain scale-150 translate-x-1/2 translate-y-1/4"
                            src="/images/sources/blue_brush_2.png"
-                           alt="" width="1000" height="0"/>
+                           alt="" fill/>
                 </div>
             </section>
         </main>
