7531892c4c973606241a63d6169a31642027b6ff
jayjeong8 <zuni4242@gmail.com>
2024-03-19 01:07:59 +0900

    feat: 메인 타이포 blur 효과 적용
    - hover시 blur 해제됨

--- app/page.tsx
+++ app/page.tsx
@@ -60,11 +60,11 @@ export default function Home() {
                 </section>
                 <section className="flex flex-col items-center justify-center h-full">
                     <div className="flex-1 flex flex-col items-center justify-center ko-font">
-                        <h1 className="text-9xl scale-125 tracking-tighter mb-20">
+                        <h1 className="text-9xl scale-125 tracking-tighter mb-20 blur-md hover:blur-none transition duration-700 ease-out">
                             <div>선 위의</div>
                             <div>여성들</div>
                         </h1>
-                        <p className="text-4xl tracking-tighter text-center whitespace-pre-line ">
+                        <p className="text-4xl tracking-tighter text-center whitespace-pre-line blur hover:blur-none transition duration-700 ease-out">
                             {'"훌륭한 그림은\n방금 일어난 일처럼 보인다."'}
                         </p>
                     </div>
