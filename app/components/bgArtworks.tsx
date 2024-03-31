87cb10b11605014c1598d1b432dfb83a3bad1b6b
jayjeong8 <zuni4242@gmail.com>
2024-03-31 20:28:32 +0900

    refactor: 배경 아트워크 컴포넌트에서 낱개 이미지 컴포넌트 분리
    - 다른 컴포넌트에서 재사용하기 위해 분리

--- app/components/bgArtworks.tsx
+++ app/components/bgArtworks.tsx
@@ -1,10 +1,4 @@
-import Image from 'next/image';
-
-const ArtworkImage = ({ className, src }: { className: string; src: string }) => (
-    <div className={`relative ${className}`}>
-        <Image className="object-contain" src={`/images/sources/${src}`} alt="" fill />
-    </div>
-);
+import ArtworkImage from '@/app/components/artworkImage';
 
 export default function BgArtworks() {
     return (
@@ -12,49 +6,49 @@ export default function BgArtworks() {
             <section className="grid grid-cols-3 grid-rows-3 w-full h-full absolute">
                 <ArtworkImage
                     className="col-start-1 col-span-1 row-start-1 row-span-1 scale-150 -translate-y-2/3"
-                    src="mix_brush.png"
+                    src="sources/mix_brush.png"
                 />
                 <ArtworkImage
                     className="col-start-3 col-span-1 row-start-1 row-span-1 scale-150 translate-x-1/4 -translate-y-1/3"
-                    src="blue_brush_3.png"
+                    src="sources/blue_brush_3.png"
                 />
                 <ArtworkImage
                     className="col-start-1 col-span-1 row-start-2 row-span-1 scale-150 -translate-x-1/3"
-                    src="blue_brush.png"
+                    src="sources/blue_brush.png"
                 />
                 <ArtworkImage
                     className="col-start-3 col-span-1 row-start-2 row-span-1 scale-150 translate-y-1/4"
-                    src="green_brush.png"
+                    src="sources/green_brush.png"
                 />
                 <ArtworkImage
                     className="col-start-1 col-span-1 row-start-3 row-span-1 scale-150 -translate-x-2/3 translate-y-2/3"
-                    src="pink_brush.png"
+                    src="sources/pink_brush.png"
                 />
                 <ArtworkImage
                     className="col-start-2 col-span-1 row-start-3 row-span-1 scale-150 -translate-x-1/2 translate-y-1/4"
-                    src="yellow_brush.png"
+                    src="sources/yellow_brush.png"
                 />
                 <ArtworkImage
                     className="col-start-3 col-span-1 row-start-3 row-span-1 scale-150 translate-x-1/2 translate-y-1/4"
-                    src="blue_brush_2.png"
+                    src="sources/blue_brush_2.png"
                 />
             </section>
             <section className="grid grid-cols-3 grid-rows-3 w-full h-full absolute">
                 <ArtworkImage
                     className="col-start-1 col-span-2 row-start-1 row-span-2 scale-150 -translate-y-1/4"
-                    src="line_1.png"
+                    src="sources/line_1.png"
                 />
                 <ArtworkImage
                     className="col-start-3 col-span-1 row-start-1 row-span-2 scale-150 translate-x-1/2"
-                    src="line_2.png"
+                    src="sources/line_2.png"
                 />
                 <ArtworkImage
                     className="col-start-1 col-span-1 row-start-3 row-span-1 scale-150 -translate-x-1/2"
-                    src="line_3.png"
+                    src="sources/line_3.png"
                 />
                 <ArtworkImage
                     className="col-start-2 col-span-3 row-start-3 row-span-1 scale-150 translate-y-1/3"
-                    src="line_4.png"
+                    src="sources/line_4.png"
                 />
             </section>
         </>
