e1ce6c8e569293aa297777872fb19fcc11c9bcf2
jayjeong8 <zuni4242@gmail.com>
2024-05-15 23:56:54 +0900

    refactor: 캔바스 모달 컴포넌트 분리

---
+++ app/components/canvasModal.tsx
@@ -0,0 +1,21 @@
+export default function CanvasModal() {
+    return (
+        <section className="w-screen h-screen fixed inset-0 z-10">
+            <div className="dimmed bg-black/30 w-full h-full absolute" />
+            <section className="flex w-1/2 h-2/3 absolute inset-0 m-auto p-2 gap-2 bg-white rounded shadow-lg">
+                <div className="w-auto h-full aspect-2/3 rounded border-2 border-gray-400">
+                    <canvas width="400" height="800" className="w-full h-full" />
+                </div>
+                <div className="w-full h-full">
+                    <textarea
+                        placeholder="당신의 이야기를 들려주세요."
+                        rows={7}
+                        required
+                        wrap="hard"
+                        className="w-full p-2 rounded border-2 border-gray-400 ko-font"
+                    />
+                </div>
+            </section>
+        </section>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import BgArtworks from '@/app/components/bgArtworks';
 import MainTypographies from '@/app/components/mainTypographies';
 import MainArts from '@/app/components/mainArts';
+import CanvasModal from '@/app/components/canvasModal';
 
 export default function Home() {
     return (
@@ -10,23 +11,7 @@ export default function Home() {
                 <MainTypographies />
                 <MainArts />
             </section>
-            <section className="w-screen h-screen fixed inset-0 z-10">
-                <div className="dimmed bg-black/30 w-full h-full absolute" />
-                <section className="flex w-1/2 h-2/3 absolute inset-0 m-auto p-2 gap-2 bg-white rounded shadow-lg">
-                    <div className="w-auto h-full aspect-2/3 rounded border-2 border-gray-400">
-                        <canvas width="400" height="800" className="w-full h-full" />
-                    </div>
-                    <div className="w-full h-full">
-                        <textarea
-                            placeholder="당신의 이야기를 들려주세요."
-                            rows={7}
-                            required
-                            wrap="hard"
-                            className="w-full p-2 rounded border-2 border-gray-400 ko-font"
-                        />
-                    </div>
-                </section>
-            </section>
+            <CanvasModal />
         </main>
     );
 }
