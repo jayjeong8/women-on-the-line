430537dfc18a883b9de86b5573944fc2f44e399b
jayjeong8 <zuni4242@gmail.com>
2024-05-15 22:18:06 +0900

    design: 캔바스 모달 레이아웃 및 스타일

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,17 @@ export default function Home() {
                 <MainTypographies />
                 <MainArts />
             </section>
+            <section className="w-screen h-screen fixed inset-0 z-10">
+                <div className="dimmed bg-black/30 w-full h-full absolute" />
+                <section className="flex w-1/2 h-2/3 absolute inset-0 m-auto p-2 gap-2 bg-white rounded shadow-lg">
+                    <div className="w-auto h-full aspect-2/3 rounded border-2 border-gray-400">
+                        <canvas />
+                    </div>
+                    <div className="w-full h-full">
+                        <textarea className="w-full h-1/2 p-2 rounded border-2 border-gray-400" />
+                    </div>
+                </section>
+            </section>
         </main>
     );
 }
